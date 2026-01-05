600b27fbfee503ba8db785da3d21def91217fc81
kregor <kregorjeager@gmail.com>
2026-01-05 19:27:05 +0800

    Add test to check if piece has character attr

--- spec/chesss_piece_spec.rb
+++ spec/chesss_piece_spec.rb
@@ -6,13 +6,17 @@ describe ChessPiece do
   describe '#initialize' do
     context 'pawn' do
       let(:role) { String.new('pawn') }
-      let(:team) { String.new('white') }
-      subject(:pawn) { described_class.new(role, team) }
+      let(:white) { String.new('white') }
+      let(:black) { String.new('black') }
+      subject(:pawn) { described_class.new(role, white) }
       it 'has role attr' do
         expect(pawn.role).to eq(role)
       end
       it 'has team attr' do
-        expect(pawn.team).to eq(team)
+        expect(pawn.team).to eq(white)
+      end
+      it 'has white character attr' do
+        expect(pawn.character).to eq("\u2659")
       end
     end
   end
