47bbdf1cc66f9f2732a15aa1ad00f01bd5ec0a90
kregor <kregorjeager@gmail.com>
2026-01-05 20:57:18 +0800

    Update character assignement to handle all pieces
    The earlier version only gives black or white pawn
    character. Now it gives character as per role

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -7,6 +7,20 @@ class ChessPiece
   def initialize(role, team)
     @role = role
     @team = team
-    @character = team == 'white' ? "\u2659" : "\u265F"
+    @character = get_character(role, team)
+  end
+
+  def get_character(role, team)
+    char = characters.select { |k, v| role == k }
+    team == 'white' ? char[role].at(0) : char[role].at(1)
+  end
+
+  def characters
+    { 'pawn' => ["\u2659", "\u265F"],
+      'rook' => ["\u2656", "\u265C"],
+      'bishop' => ["\u2657", "\u265D"],
+      'knight' => ["\u2658", "\u265E"],
+      'king' => ["\u2654", "\u265A"],
+      'queen' => ["\u2655", "\u265B"] }
   end
 end
