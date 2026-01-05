b696e5164a0df48d2af3aacbfc336119d5b3d29d
kregor <kregorjeager@gmail.com>
2026-01-05 21:05:41 +0800

    Replicate test to all pieces

--- spec/chesss_piece_spec.rb
+++ spec/chesss_piece_spec.rb
@@ -54,5 +54,81 @@ describe ChessPiece do
         expect(black_piece.character).to eq("\u265C")
       end
     end
+    context 'bishop' do
+      let(:role) { String.new('bishop') }
+      let(:white) { String.new('white') }
+      let(:black) { String.new('black') }
+      subject(:white_piece) { described_class.new(role, white) }
+      subject(:black_piece) { described_class.new(role, black) }
+      it 'has role attr' do
+        expect(white_piece.role).to eq(role)
+      end
+      it 'has team attr' do
+        expect(white_piece.team).to eq(white)
+      end
+      it 'has white character attr' do
+        expect(white_piece.character).to eq('♗')
+      end
+      it 'has black character attr' do
+        expect(black_piece.character).to eq('♝')
+      end
+    end
+    context 'knight' do
+      let(:role) { String.new('knight') }
+      let(:white) { String.new('white') }
+      let(:black) { String.new('black') }
+      subject(:white_piece) { described_class.new(role, white) }
+      subject(:black_piece) { described_class.new(role, black) }
+      it 'has role attr' do
+        expect(white_piece.role).to eq(role)
+      end
+      it 'has team attr' do
+        expect(white_piece.team).to eq(white)
+      end
+      it 'has white character attr' do
+        expect(white_piece.character).to eq('♘')
+      end
+      it 'has black character attr' do
+        expect(black_piece.character).to eq('♞')
+      end
+    end
+    context 'queen' do
+      let(:role) { String.new('queen') }
+      let(:white) { String.new('white') }
+      let(:black) { String.new('black') }
+      subject(:white_piece) { described_class.new(role, white) }
+      subject(:black_piece) { described_class.new(role, black) }
+      it 'has role attr' do
+        expect(white_piece.role).to eq(role)
+      end
+      it 'has team attr' do
+        expect(white_piece.team).to eq(white)
+      end
+      it 'has white character attr' do
+        expect(white_piece.character).to eq('♕')
+      end
+      it 'has black character attr' do
+        expect(black_piece.character).to eq('♛')
+      end
+    end
+    context 'king' do
+      let(:role) { String.new('king') }
+      let(:white) { String.new('white') }
+      let(:black) { String.new('black') }
+      subject(:white_piece) { described_class.new(role, white) }
+      subject(:black_piece) { described_class.new(role, black) }
+      it 'has role attr' do
+        expect(white_piece.role).to eq(role)
+      end
+      it 'has team attr' do
+        expect(white_piece.team).to eq(white)
+      end
+      it 'has white character attr' do
+        expect(white_piece.character).to eq('♔')
+      end
+      it 'has black character attr' do
+        expect(black_piece.character).to eq('♚')
+      end
+    end
   end
 end
