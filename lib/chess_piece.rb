b24264be7b1b0ecefe526c17eeae352de42dd1b1
kregor <kregorjeager@gmail.com>
2026-01-05 19:28:47 +0800

    Add character attr that corresponds to team

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -2,10 +2,11 @@
 
 # Create an instance of a chesspiece given it's role and team
 class ChessPiece
-  attr_reader :role, :team
+  attr_reader :role, :team, :character
 
   def initialize(role, team)
     @role = role
     @team = team
+    @character = team == 'white' ? "\u2659" : "\u265F"
   end
 end
