bcec4fde431b014e0a36ecfbcd874117e47c4620
kregor <kregorjeager@gmail.com>
2026-01-05 19:15:01 +0800

    Add code and spec that gives piece a role

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal:false
+
+# Create an instance of a chesspiece given it's role and team
+class ChessPiece
+  attr_reader :role
+
+  def initialize(role, team)
+    @role = role
+  end
+end
--- spec/chesss_piece_spec.rb
+++ spec/chesss_piece_spec.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal:false
 
 require './lib/chess_piece'
+
+describe ChessPiece do
+  describe '#initialize' do
+    context 'pawn' do
+      let(:role) { String.new('pawn') }
+      let(:team) { String.new('white') }
+      subject(:pawn) { described_class.new(role, team) }
+      it 'has role attr' do
+        expect(pawn.role).to eq(role)
+      end
+    end
+  end
+end
