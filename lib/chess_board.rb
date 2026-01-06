29948373be676a6ed1aac9cd48d374192c1ff763
kregor <kregorjeager@gmail.com>
2026-01-06 22:54:58 +0800

    Add method to set all pieces in default position

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -15,4 +15,33 @@ class ChessBoard
   def init_chesspiece(role, pos, team)
     @board[pos[0]][pos[1]] = ChessPiece.new(role, team)
   end
+
+  def set_board_to_default
+    set_pawns
+    set_rook_knight_bishop
+    set_royals
+  end
+
+  def set_pawns
+    (0..7).each do |i|
+      @board[1][i] = ChessPiece.new('pawn', 'white')
+      @board[6][i] = ChessPiece.new('pawn', 'black')
+    end
+  end
+
+  def set_rook_knight_bishop
+    [0, 7].each do |i|
+      team = i.zero? ? 'white' : 'black'
+      [0, 7].each { |j| @board[i][j] = ChessPiece.new('rook', team) }
+      [1, 6].each { |j| @board[i][j] = ChessPiece.new('knight', team) }
+      [2, 5].each { |j| @board[i][j] = ChessPiece.new('bishop', team) }
+    end
+  end
+
+  def set_royals
+    @board[0][3] = ChessPiece.new('king', 'white')
+    @board[0][4] = ChessPiece.new('queen', 'white')
+    @board[7][3] = ChessPiece.new('king', 'black')
+    @board[7][4] = ChessPiece.new('queen', 'black')
+  end
 end
