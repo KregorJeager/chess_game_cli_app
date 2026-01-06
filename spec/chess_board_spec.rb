6484a764fb9f4a17fd0eca4c21aef16895c910ee
kregor <kregorjeager@gmail.com>
2026-01-06 19:28:58 +0800

    Update @turn attr test
    Prior test triggers false_positive warning from rs
    pec.

--- spec/chess_board_spec.rb
+++ spec/chess_board_spec.rb
@@ -12,10 +12,10 @@ describe ChessBoard do
       expect { board.board = [1] }.to raise_error(an_instance_of(NoMethodError))
     end
     it 'has @turn' do
-      expect { board.turn }.not_to raise_error(an_instance_of(NoMethodError))
+      expect(board.turn).to eq('white')
     end
     it '@turn has attr accesor' do
-      expect { board.turn = 'black' }.not_to raise_error(an_instance_of(NoMethodError))
+      expect { board.turn = 'black' }.to change { board.turn }.to('black')
     end
   end
 end
