bfe89c8fb41e62ede0e9656a174e71939194d955
kregor <kregorjeager@gmail.com>
2026-01-06 21:24:09 +0800

    Add #init_chesspiece and require ChessPiece

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: false
 
+require './lib/chess_piece'
+
 # comment
 class ChessBoard
   attr_reader :board
@@ -8,5 +10,9 @@ class ChessBoard
   def initialize
     @board = Array.new(8) { Array.new(8) { nil } }
     @turn = 'white'
+  end
+
+  def init_chesspiece(role, pos, team)
+    @board[pos[0]][pos[1]] = ChessPiece.new(role, team)
   end
 end
