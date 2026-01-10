196177c1194f1d378f05dd78b02b6222e6078a46
kregor <kregorjeager@gmail.com>
2026-01-10 15:45:17 +0800

    Add polarity to determine whether how to move
    white pieces moves from 0 to 7 while black moves
    7 to 0. polarity sets if we add or subtract move

--- lib/rule_checkers.rb
+++ lib/rule_checkers.rb
@@ -5,11 +5,12 @@ module RuleCheckers
     i = cur[0]
     j = cur[1]
     piece = @board[i][j]
-    moves = [[i + 1, j]]
-    moves << [i + 1, j - 1] if enemy?([i + 1, j - 1], piece.team)
-    moves << [i + 1, j + 1] if enemy?([i + 1, j + 1], piece.team)
+    polarity = piece.team == 'white' ? 1 : -1
+    moves = [[i + 1 * polarity, j]]
+    moves << [i + 1 * polarity, j - 1] if enemy?([i + 1 * polarity, j - 1], piece.team)
+    moves << [i + 1 * polarity, j + 1] if enemy?([i + 1 * polarity, j + 1], piece.team)
     if i == 1 && piece.team == 'white' || i == 6 && piece.team == 'black'
-      moves << [i + 2, j]
+      moves << [i + 2 * polarity, j]
       return moves.include?(new)
     end
 
