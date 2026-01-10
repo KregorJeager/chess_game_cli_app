ef58a6efb63e1aabc8ab00cd2b2cc85fb9ae8fdc
kregor <kregorjeager@gmail.com>
2026-01-10 15:44:49 +0800

    Add same test for #pawn_valid? but black pieces

--- spec/rule_checkers_spec.rb
+++ spec/rule_checkers_spec.rb
@@ -41,6 +41,42 @@ describe ChessBoard do
           expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
         end
       end
+      context 'work with black pieces' do
+        it 'eats enemy on the sides' do
+          current_position = [6, 0]
+          new_position = [5, 1]
+          output = [true, "moved pawn from #{current_position} to #{new_position}"]
+          checkers.board[6][0] = ChessPiece.new('pawn', 'black')
+          checkers.board[5][1] = ChessPiece.new('pawn', 'white')
+          expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+        end
+        context "pawn hasn't made a move yet" do
+          it 'allows one step forward' do
+            current_position = [6, 0]
+            new_position = [4, 0]
+            output = [true, "moved pawn from #{current_position} to #{new_position}"]
+            checkers.board[6][0] = ChessPiece.new('pawn', 'black')
+            expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+          end
+          it 'allows two step forward' do
+            current_position = [6, 0]
+            new_position = [4, 0]
+            output = [true, "moved pawn from #{current_position} to #{new_position}"]
+            checkers.board[6][0] = ChessPiece.new('pawn', 'black')
+            expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+          end
+        end
+
+        context 'pawn already made a move' do
+          it 'only allows 1 step forward' do
+            current_position = [5, 0]
+            new_position = [3, 0]
+            output = [false, 'Invalid move (only 1 step forward)']
+            checkers.board[5][0] = ChessPiece.new('pawn', 'white')
+            expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+          end
+        end
+      end
     end
   end
 end
