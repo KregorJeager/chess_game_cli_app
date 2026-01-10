43d032faaae2ce18b59fa6b3cfb7ae6b31949e03
kregor <kregorjeager@gmail.com>
2026-01-10 14:42:18 +0800

    UPdate add test for pawn_valid
    Added context for the already existing test and
    add pawn eating enemy

--- spec/rule_checkers_spec.rb
+++ spec/rule_checkers_spec.rb
@@ -7,20 +7,39 @@ describe ChessBoard do
     subject(:checkers) { described_class.new }
 
     describe '#pawn_valid?' do
-      it 'allows two step forward on first move' do
+      it 'eats enemy on the sides' do
         current_position = [1, 0]
-        new_position = [3, 0]
+        new_position = [2, 1]
         output = [true, "moved pawn from #{current_position} to #{new_position}"]
         checkers.board[1][0] = ChessPiece.new('pawn', 'white')
+        checkers.board[2][1] = ChessPiece.new('pawn', 'black')
         expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
       end
+      context "pawn hasn't made a move yet" do
+        it 'allows one step forward' do
+          current_position = [1, 0]
+          new_position = [2, 0]
+          output = [true, "moved pawn from #{current_position} to #{new_position}"]
+          checkers.board[1][0] = ChessPiece.new('pawn', 'white')
+          expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+        end
+        it 'allows two step forward' do
+          current_position = [1, 0]
+          new_position = [3, 0]
+          output = [true, "moved pawn from #{current_position} to #{new_position}"]
+          checkers.board[1][0] = ChessPiece.new('pawn', 'white')
+          expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+        end
+      end
 
-      it 'only allows 1 step forward after the initial move' do
-        current_position = [2, 0]
-        new_position = [4, 0]
-        output = [false, 'Invalid move (only 1 step forward)']
-        checkers.board[2][0] = ChessPiece.new('pawn', 'white')
-        expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+      context 'pawn already made a move' do
+        it 'only allows 1 step forward' do
+          current_position = [2, 0]
+          new_position = [4, 0]
+          output = [false, 'Invalid move (only 1 step forward)']
+          checkers.board[2][0] = ChessPiece.new('pawn', 'white')
+          expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
+        end
       end
     end
   end
