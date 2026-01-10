fa333188a08b092c5f87746cd565cf1731376ad8
kregor <kregorjeager@gmail.com>
2026-01-10 17:10:47 +0800

    Refactor and add documentation to #rook_path

--- lib/rule_checkers.rb
+++ lib/rule_checkers.rb
@@ -42,13 +42,23 @@ module RuleCheckers
     # straight moves should have one common coordinate
     return nil unless cur[0] == new[0] || cur[1] == new[1]
 
-    # determine which coordinates changes
-    axis = cur[0] == new[0] ? 1 : 0
-    polarity = cur[axis] > new[axis] ? -1 : 1
+    # returns 0 or 1, depends on which axis is moving | cur[axis0][axis1]
+    axis = rook_axis(cur, new)
+    # return + or - 1, depends when we need to add or subtrct from cur to
+    # reach new
+    polarity = rook_polarity(cur, new)
     path = []
     until cur[axis] == new[axis]
       cur[axis] += 1 * polarity
       path << [cur[0], cur[1]]
     end
+  end
+
+  def rook_axis(cur, new)
+    cur[0] == new[0] ? 1 : 0
+  end
+
+  def rook_polarity(cur, new)
+    cur[axis] > new[axis] ? -1 : 1
   end
 end
