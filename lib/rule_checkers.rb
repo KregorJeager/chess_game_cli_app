9681453834bed6e2a6828b70d1069ee63407d8a0
kregor <kregorjeager@gmail.com>
2026-01-10 17:26:28 +0800

    Fix #rook_path not returning nil
    the func variable ``path`` was not being returned
    and axis was not available to #rook_polarity

--- lib/rule_checkers.rb
+++ lib/rule_checkers.rb
@@ -46,19 +46,20 @@ module RuleCheckers
     axis = rook_axis(cur, new)
     # return + or - 1, depends when we need to add or subtrct from cur to
     # reach new
-    polarity = rook_polarity(cur, new)
+    polarity = rook_polarity(cur, new, axis)
     path = []
     until cur[axis] == new[axis]
       cur[axis] += 1 * polarity
       path << [cur[0], cur[1]]
     end
+    path
   end
 
   def rook_axis(cur, new)
     cur[0] == new[0] ? 1 : 0
   end
 
-  def rook_polarity(cur, new)
+  def rook_polarity(cur, new, axis)
     cur[axis] > new[axis] ? -1 : 1
   end
 end
