89b18bc47bfe0201e4f596aeae48fbb4d4edccce
kregor <kregorjeager@gmail.com>
2026-01-10 15:35:54 +0800

    Update #valid_pawn? to allow eating pieces on side

--- lib/rule_checkers.rb
+++ lib/rule_checkers.rb
@@ -5,10 +5,22 @@ module RuleCheckers
     i = cur[0]
     j = cur[1]
     piece = @board[i][j]
-    return [[i + 1, j], [i + 2, j]].include?(new) if
-    i == 1 && piece.team == 'white' || i == 6 && piece.team == 'black'
+    moves = [[i + 1, j]]
+    moves << [i + 1, j - 1] if enemy?([i + 1, j - 1], piece.team)
+    moves << [i + 1, j + 1] if enemy?([i + 1, j + 1], piece.team)
+    if i == 1 && piece.team == 'white' || i == 6 && piece.team == 'black'
+      moves << [i + 2, j]
+      return moves.include?(new)
+    end
 
-    [i + 1, j].include?(new)
+    moves.include?(new)
+  end
+
+  def enemy?(pos, team)
+    piece = @board[pos[0]][pos[1]]
+    return false if piece.nil?
+
+    piece.team != team
   end
 
   def pawn_next_moves(cur)
