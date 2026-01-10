ad198e41455a4792c2aa025a571119bb82f6d9bf
kregor <kregorjeager@gmail.com>
2026-01-10 14:22:38 +0800

    Add algorithm for pawn_valid

--- lib/rule_checkers.rb
+++ lib/rule_checkers.rb
@@ -2,6 +2,14 @@
 
 module RuleCheckers
   def pawn_valid?(cur, new)
+    i = cur[0]
+    j = cur[1]
+    piece = @board[cur[0]][cur[1]]
+    if cur[0] == 1 && piece.team == 'white' || cur[0] == 6 && piece.team == 'black'
+      return [[i + 1, j], [i + 2, j]].include?(new)
+    end
+
+    false
   end
 
   def pawn_next_moves(cur)
--- spec/rule_checkers_spec.rb
+++ spec/rule_checkers_spec.rb
@@ -11,7 +11,7 @@ describe ChessBoard do
         current_position = [1, 0]
         new_position = [3, 0]
         output = [true, "moved pawn from #{current_position} to #{new_position}"]
-        expect(checkers.pawn_valid?(current_position, new_position)).to eq(output)
+        expect(checkers.pawn_valid?(current_position, new_position)).to eq(output[0])
       end
 
       it 'only allows 1 step forward after the initial move' do
