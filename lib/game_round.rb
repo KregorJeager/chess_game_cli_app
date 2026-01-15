affd71f508787e3635b35f0d9972c28194c3d680
kregor <kregorjeager@gmail.com>
2026-01-15 15:31:10 +0800

    Added submethods for #filter_input

--- lib/game_round.rb
+++ lib/game_round.rb
@@ -13,7 +13,9 @@ class GameRound
   end
 
   def make_move(board)
+    print_board
     loop do
+      print "#{@chess.turn} to move; enter move here =>"
       input = player_input
       return @chess.move_piece(input[0], input[1]) if @chess.valid?(input[0], input[1])
 
@@ -31,6 +33,17 @@ class GameRound
       return [input[0].split(',').map!(&:to_i), input[1].split(',').map!(&:to_i)]
     end
     input
+  end
+
+  def coordinate_format?(str)
+    return false unless str.length == 7
+
+    coor?(str[0]) && str[1] == ',' && coor?(str[2]) && str[3] == '|' &&
+      coor?(str[4]) && str[5] == ',' && coor?(str[6])
+  end
+
+  def coor?(str)
+    %w[0 1 2 3 4 5 6 7].include?(str)
   end
 
   def print_board
