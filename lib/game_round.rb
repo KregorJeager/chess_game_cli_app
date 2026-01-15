58c29ea7b2b27a9d43c29b51e5a3511a5411f3b7
kregor <kregorjeager@gmail.com>
2026-01-15 11:08:53 +0800

    Add method to get player input

--- lib/game_round.rb
+++ lib/game_round.rb
@@ -5,4 +5,16 @@ require 'lib/chess_board'
 class GameRound
   def initialize
   end
+
+  def player_input
+    filter_input(gets.chomp)
+  end
+
+  def filter_input(input)
+    if coordinate_format?(input)
+      input = input.split('|')
+      return [input[0].split(',').map!(&:to_i), input[1].split(',').map!(&:to_i)]
+    end
+    input
+  end
 end
